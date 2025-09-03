8a87891d29bee53a8b676ed881d52d93ddf1f5cc
Salma <salmabarbara1108@gmail.com>
2025-09-03 16:20:42 +0100

    move routes to separate file +load .env file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv 
+load_dotenv('.env')
+
+from routes import base
+
+
 
 app =FastAPI()
 
-
-@app.get("/")
-async def welcome():
-    return{
-        "message":"Welcome to mini-rag"
-    }
+app.include_router(base.base_router)
